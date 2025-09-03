f9f6ff1b1f09f619b1f98115f7f1dbcd5df01488
my first github repo <mrarifmondol98@gmail.com>
2025-09-03 12:15:34 -0800

    Added function for heart option in each card items

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
+// Functaion Heart Click
 
-document.getElementById('btn-heart-click').addEventListener('click', function(){
+function heartClicked(id){
+    let liked = false;
+document.getElementById(id).addEventListener('click', function () {
     const heartEl = document.getElementById('available-heart');
+      let count = parseInt(heartEl.innerText);
 
-    const currentCount = parseInt(heartEl.innerText, 10);
+      if (liked === false) {
+        heartEl.innerText = count + 1; 
+        liked = true;
+      } else {
+        heartEl.innerText = count - 1; 
+        liked = false;
+      }
+});
+}
 
-    if (currentCount === 0) {
-      heartEl.innerText = 1; // Add 1
-    } else {
-      heartEl.innerText = 0; // Remove 1
-    }
-    
-    
-    
-})
+heartClicked('btn-heart-item1');
+heartClicked('btn-heart-item2');
+heartClicked('btn-heart-item3');
+heartClicked('btn-heart-item4');
+heartClicked('btn-heart-item5');
+heartClicked('btn-heart-item6');
+heartClicked('btn-heart-item7');
+heartClicked('btn-heart-item8');
+heartClicked('btn-heart-item9');
+
+
+
